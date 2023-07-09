fdad4a822a82f6858b61fe1a88d82a27ef278275
Chet25 <chetan25december@gmail.com>
2023-07-09 17:35:41 +0530

    july 9 variable and viewport height changes

--- app/js/script.js
+++ app/js/script.js
@@ -6,30 +6,30 @@ const fadeElems = document.querySelectorAll('.has-fade');
 const body = document.querySelector('body');
 
 
-btnHamburger.addEventListener('click', function(){
-    console.log('open hamburger');
-    if(header.classList.contains('open')){ 
-      //close hamburger menu
-      body.classList.remove('noscroll');
-      header.classList.remove('open');
-      fadeElems.forEach(function(element){
-      
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out'); 
-    });
-    }
-    else {
-      //open hamburger menu
-      body.classList.add('noscroll');
-      header.classList.add('open'); 
-      fadeElems.forEach(function(element){
-        element.classList.remove('fade-out');
-        element.classList.add('fade-in');
-  
+btnHamburger.addEventListener('click', function () {
+  console.log('open hamburger');
+  if (header.classList.contains('open')) {
+    //close hamburger menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElems.forEach(function (element) {
 
-      });
-     
-    }
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+  }
+  else {
+    //open hamburger menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+
+
+    });
+
+  }
 })
 
 /*const body = document.querySelector('body');
